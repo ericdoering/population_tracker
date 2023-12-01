75f86cd21e722f4bdaeda9fce117096b1c933025
Eric Doering <ericbenjamindoering@gmail.com>
2023-12-01 18:05:09 -0500

    successfully displaying SF data on graph

--- D3_SF_script.js
+++ D3_SF_script.js
@@ -45,24 +45,137 @@ let dataset = [
 d3.json("population_data/San_Francisco-population.json").then(function (data){
 
 
-    let mergedDataset = dataset.map((existingDataPoint) => {
-        // Find the corresponding population data point with the same date
-        let matchingPopulationData = newPopulationData.find(
-          (newDataPoint) => newDataPoint.Date === existingDataPoint.date.toISOString().split('T')[0]
-        );
-      
-        // If a matching population data point is found, update the population value
-        if (matchingPopulationData) {
-          existingDataPoint.population = matchingPopulationData.Population;
-        }
-      
-        return existingDataPoint;
-      });
+  let population = data.map(entry => entry.Population);
+  let date = data.map(entry => entry.Date);
 
-    // let updatedDataset = dataset.map((entry, index) => ({
-    //     date: new Date(date[index]),
-    //     population: parseInt(population[index], 10),
-    //   }));
+  let updatedDataset = dataset.map((entry, index) => ({
+    date: new Date(date[index]),
+    population: parseInt(population[index], 10),
+  }));
 
- 
+  console.log(population)
+
+  // setting y axis scale
+
+x.domain(d3.extent(updatedDataset, d => d.date));
+y.domain([500000, d3.max(updatedDataset, d => d.population) + 1000000]);
+
+
+svg.append("g")
+  .attr("transform", `translate(0,${height})`)
+  .style("font-size", "14px")
+  .call(d3.axisBottom(x)
+    .ticks(d3.timeYear.every(5)) 
+    .tickFormat(d3.timeFormat("%Y")))
+    .call(g => g.select(".domain").remove())
+    .selectAll(".tick line")
+    .style("stroke-opacity", 0)
+    svg.selectAll(".tick text")
+    .attr("fill", "#777"); 
+
+
+// the y axis 
+
+svg.append("g")
+  .style("font-size", "10px")
+  .call(d3.axisLeft(y)
+  .tickSize(0)
+  .tickPadding(10))
+  .call(g => g.select(".domain").remove())
+  .selectAll(".tick text")
+  .style("fill", "#777")
+  .style("visibility", (d, i, nodes) => {
+    if(i === 0){
+      return "hidden"
+    }
+    else {
+      return "visible"
+    }
+  });
+
+
+  // vertical graph lines
+
+svg.selectAll("xGrid")
+.data(x.ticks().slice(1))
+.join("line")
+.attr("x1", d => x(d))
+.attr("x2", d => x(d))
+.attr("y1", 0)
+.attr("y2", height)
+.attr("stroke", "#e0e0e0")
+.attr("stroke-width", .5)
+
+// horizontal graph lines
+
+svg.selectAll("yGrid")
+.data(y.ticks().slice(1))
+.join("line")
+.attr("x1", 0)
+.attr("x2", width)
+.attr("y1", d => y(d))
+.attr("y2", d => y(d))
+.attr("stroke", "#e0e0e0")
+.attr("stroke-width", .5)
+
+// title of the graph
+
+svg.append("text")
+.attr("class", "chart-title")
+.attr("x", margin.right + 200)
+.attr("y", margin.top - 100)
+.style("font-size", "24px")
+.style("font-weight", "bold")
+.style("font-family", "sans-serif")
+.text("Historical and Projected Population of San Francisco (1950 - 2040)")
+
+// adding y axis title
+
+svg.append("text")
+.attr("transform", "rotate(-90)")
+.attr("y", 0 - margin.left)
+.attr("x", 0 - (height / 2))
+.attr("dy", "1em")
+.style("text-anchor", "middle")
+.style("font-size", "14px")
+.style("fill", "#777")
+.style("font-family", "sans-serif")
+.text("Total Population")
+
+// adding x axis title
+
+svg.append("text")
+    .attr("class", "x label")
+    .attr("text-anchor", "middle")
+    .attr("x", width - 570)
+    .attr("y", height + 40)
+    .style("fill", "#777")
+    .style("font-size", "14px")
+    .style("font-family", "sans-serif")
+    .text("Year");
+
+  // adding source credit
+
+svg.append("text")
+.attr("class", "source-credit")
+.attr("x", width - 1150)
+.attr("y", height + margin.bottom + 0.1)
+.style("font-size", "7px")
+.style("font-family", "sans-serif")
+.text("Source: https://data.cityofnewyork.us/")
+
+
+let line = d3.line()
+  .x(d => x(d.date))
+  .y(d => y(d.population));
+
+// plotting the line of the graph
+
+svg.append("path")
+  .datum(updatedDataset)
+  .attr("fill", "none")
+  .attr("stroke", "royalblue")
+  .attr("stroke-width", 2)
+  .attr("d", line);
+
 })

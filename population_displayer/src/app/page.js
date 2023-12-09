3b1a3ae372344ce03246999ffc4e2fde180401e6
Eric Doering <ericbenjamindoering@gmail.com>
2023-12-09 13:27:39 -0500

    incorporating existing app into nextjs app

---
+++ population_displayer/src/app/page.js
@@ -0,0 +1,13 @@
+"use client";
+
+import { nycGraph } from "./components/nycGraph"
+
+
+export default function Home() {
+  return (
+      <>
+        <h1>Testing to see if this works</h1>
+        <nycGraph />
+      </>
+  )
+}

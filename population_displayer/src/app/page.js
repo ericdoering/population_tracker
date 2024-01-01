9f0b1c7be61bb2d3a5770ada15e3c4bb6a656e3e
Eric Doering <ericbenjamindoering@gmail.com>
2024-01-01 14:34:39 -0500

    refactoring project

--- population_displayer/src/app/page.js
+++ population_displayer/src/app/page.js
@@ -6,6 +6,7 @@ import { NewYorkGraph } from "./components/NewYorkGraph"
 export default function Home() {
   return (
       <>
+      <h1>Major City Population Data</h1>
         <NewYorkGraph />
       </>
   )
